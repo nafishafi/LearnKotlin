4a70f1eccb8992b9a3db9c353b247f6bc2c567c9
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-16 01:05:24 +0700

    add break continue

---
+++ src/main/kotlin/break-continue.kt
@@ -0,0 +1,28 @@
+package com.dicoding.kotlin
+
+fun main() {
+//    break = langsung berhenti
+//    continue = di skip
+    val listOfInt = listOf(1, 2, 3, null, 5, null, 7)
+
+    for (i in listOfInt) {
+        if (i == null) continue
+        print(i)
+    }
+    println("\n===========")
+    val listOfInt2 = listOf(1, 2, 3, null, 5, null, 7)
+
+    for (i in listOfInt2) {
+        if (i == null) break
+        print(i)
+    }
+    println("\n===========")
+    loop@ for (i in 1..10) {
+        println("Outside Loop")
+
+        for (j in 1..10) {
+            println("Inside Loop")
+            if ( j > 5) break@loop
+        }
+    }
+}
