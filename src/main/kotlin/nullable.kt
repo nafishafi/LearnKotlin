75150b6634d2986a982df6d2110fb73a3e55e7d4
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 18:38:49 +0700

    add Safe Call, elvis , dll

--- src/main/kotlin/nullable.kt
+++ src/main/kotlin/nullable.kt
@@ -9,4 +9,23 @@ fun main(){
     if(string != null) { // smart cast
         println(string.length) // It works now!
     }
+
+//    ============
+//    Safe Calls Operator
+    val text: String? = null
+    println(text?.length)
+//    ============
+//    Elvis Operator
+//    (condition ? then : else)
+    val text2: String? = null
+    val textLength = text2?.length ?: 7
+    println(textLength)
+//    Sama Seperti dibawah (IF)
+//    val textLength = if (text != null) text.length else 7
+
+//    =====================
+//     non-null ASSERTION kompiler akan mengizinkan kita untuk mengakses atau mengelola nilai dari sebuah objek nullable.
+//     Namun, penggunaan operator tersebut sangat tidak disarankan karena akan memaksa sebuah objek menjadi non-null.
+    val text3: String? = null
+    val textLength2 = text3!!.length // ready to go ???
 }
