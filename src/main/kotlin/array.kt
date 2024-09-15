8de6584ad8941982cfe82773a47d68c538f99482
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 17:53:47 +0700

    add array statement

---
+++ src/main/kotlin/array.kt
@@ -0,0 +1,20 @@
+package com.dicoding.kotlin
+
+fun main(){
+    val intArray = intArrayOf(1, 3, 5, 7)  // [1, 3, 5, 7]
+    intArray[2] = 11                       // [1, 3, 11, 7]
+
+    println(intArray[2])
+//  ====================
+//    Array yang hanya bisa dimasukkan nilai dengan tipe data Int, dll
+//    intArrayOf() : IntArray
+//    booleanArrayOf() : BooleanArray
+//    charArrayOf() : CharArray
+//    longArrayOf() : LongArray
+//    shortArrayOf() : ShortArray
+//    byteArrayOf() : ByteArray
+
+//    mix Array
+    val mixArray = arrayOf(1, 3, 5, 7 , "Dicoding" , true)
+    println(mixArray[4])
+}
