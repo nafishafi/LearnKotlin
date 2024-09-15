cac535801da06d18c8ac560c378e8839ee9c1cd8
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 17:31:29 +0700

    add print statemnet

---
+++ src/main/kotlin/print.kt
@@ -0,0 +1,14 @@
+package com.dicoding.kotlin
+
+// main function
+
+fun main() {
+    val angka : Int = 9
+    val ConstName = "Nafis" // constan
+    var name = "Nafis" // bisa diubah
+
+    print("Hello my name is ") // 1 baris
+    println(name) // ganti baris (ln)
+    println(if (true) "Always true" else "Always false")
+    print(angka)
+}
