fa31dcb235c3d3c4d18ce468f7e75f834899cf15
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 20:48:58 +0700

    add default and named argument

---
+++ src/main/kotlin/default-Argument.kt
@@ -0,0 +1,16 @@
+package com.dicoding.kotlin
+
+// Default Argument
+// Isi Variabel default untuk parameter
+
+fun getName(
+    first: String = "Kotlin",
+    middle: String = " is ",
+    last: String = "Awesome"): String {
+    return "$first $middle $last"
+}
+
+fun main() {
+    val fullName = getName(first = "Dicoding")
+    print(fullName)
+}
---
+++ src/main/kotlin/named-Argument.kt
@@ -0,0 +1,11 @@
+package com.dicoding.kotlin
+
+//  Dengan ini, kita tak perlu lagi menghafal posisi dari sebuah parameter.
+fun main() {
+    val fullName = getFullName(first = "Kotlin" , middle = " is ", last = "Awesome")
+    print(fullName)
+}
+
+fun getFullName(first: String, middle: String, last: String): String {
+    return "$first $middle $last"
+}
