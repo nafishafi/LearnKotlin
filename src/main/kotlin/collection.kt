eded4fd2e4c28b857708f4e64c075cb6f0661d7f
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 17:34:55 +0700

    add collection

---
+++ src/main/kotlin/collection.kt
@@ -0,0 +1,14 @@
+package com.dicoding.kotlin
+
+fun main(){
+    val integerList = listOf(4,2,1,3.4) // boleh sama (List)
+    val integerSet = setOf(4,2,1,3.4) // nilai unik (Set)
+    val capital = mapOf(               // key to value, key unik (TDK BOLEH SAMA) (MAP)
+        "Jakarta" to "Indonesia",
+        "London" to "England",
+        "New Delhi" to "India"
+    )
+
+    println(integerList[2])
+    println(capital["Jakarta"]) // capital.getValue("Jakarta")
+}
