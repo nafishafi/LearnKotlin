decc3acc497b4240315937227f35bd8e7f7cd36a
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-16 00:11:38 +0700

    add expression

---
+++ src/main/kotlin/expression.kt
@@ -0,0 +1,17 @@
+package com.dicoding.kotlin
+
+fun main() {
+
+    println(sum(1 , 1 * 4))
+}
+
+fun sum(value1: Int, value2: Int) = value1 + value2
+
+//fun main() {
+//    val value1 = 10
+//    val value2 = 10
+//
+//    println(sum(value1, value2))
+//}
+//
+//fun sum(value1: Int, value2: Int) = value1 + value2
---
+++ src/main/kotlin/quiz.kt
@@ -0,0 +1,7 @@
+package com.dicoding.kotlin
+
+fun main(){
+    val listOfInt = arrayOf(1,2,3,5)
+    println(listOfInt[2])
+
+}
