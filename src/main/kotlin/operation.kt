873d8cc2e255f09893d9ebb3fd345e552781b7c3
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 17:33:08 +0700

    add operation

---
+++ src/main/kotlin/operation.kt
@@ -0,0 +1,19 @@
+package com.dicoding.kotlin
+
+fun main() {
+    val firstWord = "Dicoding "
+    val lastWord = "Academy"
+    println(firstWord + lastWord)
+
+    val angka1 = 10
+    val angka2 = 5
+    println(angka1 + angka2)
+
+//    ============================
+    var value = 1
+    value = value + 2
+
+// Menjadi seperti ini.
+    var anotherValue = 1
+    anotherValue += 2
+}
