2754361e97a299a6f68f169c3c2f9e987fbe9bce
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-16 00:59:12 +0700

    add for loop

---
+++ src/main/kotlin/for-loop.kt
@@ -0,0 +1,23 @@
+package com.dicoding.kotlin
+
+fun main() {
+    val ranges = 1..5
+    for (i in ranges){
+        println("value is $i!")
+    }
+    println("\n=======\n")
+    val ranges2 = 1.rangeTo(10) step 3
+    for (i in ranges2 ){
+        println("value is $i!")
+    }
+    println("\n=======\n")
+    val ranges3 = 1.rangeTo(10) step 3
+    for ((index, value) in ranges3.withIndex()) {
+        println("value $value with index $index")
+    }
+    println("\n=======\n")
+    val ranges4 = 1.rangeTo(10) step 3
+    ranges4.forEach { value ->
+        println("value is $value!")
+    }
+}
