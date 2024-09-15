98082dbf77df34e003d2e3325ea8d4b184ff4104
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 17:35:23 +0700

    add char

---
+++ src/main/kotlin/char.kt
@@ -0,0 +1,13 @@
+package com.dicoding.kotlin
+
+fun main() {
+    var vocal = 'A'
+
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal++)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+    println("Vocal " + vocal--)
+}
