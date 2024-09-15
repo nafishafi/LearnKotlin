27fc04fb2e66771e137884146f6bd675b6cb8b6e
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 17:35:49 +0700

    add boolean statement

---
+++ src/main/kotlin/boolean_Statement.kt
@@ -0,0 +1,34 @@
+package com.dicoding.kotlin
+
+fun main(){
+    val officeOpen = 7
+    val officeClosed = 16
+    val now = 20
+//  val isOpen : Boolean
+//  && (AND)
+    val isOpen = if (now >= officeOpen && now <= officeClosed){
+        true
+    } else {
+        false
+    }
+
+    println("Office is open : $isOpen")
+//  ============
+//    SIMPEL
+//    val isOpen = now >= officeOpen && now <= officeClosed
+//
+//    print("Office is open : $isOpen"
+
+//    ===========
+//    || (OR)
+    val isClose = now < officeOpen || now > officeClosed
+
+    println("Office is closed : $isClose")
+//    ===========
+//    ! (NOT)
+    if (!isOpen) {
+        print("Office is closed")
+    } else {
+        print("Office is open")
+    }
+}
