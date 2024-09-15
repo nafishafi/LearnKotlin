29bd7671142b280f0c0feb1c1f35ce29d7304c7b
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-15 17:34:15 +0700

    add if statement

---
+++ src/main/kotlin/if_Statement.kt
@@ -0,0 +1,39 @@
+package com.dicoding.kotlin
+
+fun main(){
+    val openHours = 7
+    val now = 20
+    val office: String
+    if (now > openHours) {
+        office = "Office already open"
+    } else {
+        office = "Office is closed"
+    }
+
+    println(office)
+
+//    val openHours = 7
+//    val now = 20
+//    val office: String
+//    office = if (now > openHours) {
+//        "Office already open"
+//    } else {
+//        "Office is closed"
+//    }
+//
+//    print(office)
+
+//    Nested If
+    val jamBuka = 7
+    val sekarang = 7
+    val kantor: String
+    kantor = if (sekarang > 7) {
+        "Office already open"
+    } else if (now == jamBuka){
+        "Wait a minute, office will be open"
+    } else {
+        "Office is closed"
+    }
+
+    print(office)
+}
