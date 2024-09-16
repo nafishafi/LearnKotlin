c896c20ee33fd8712317aed710d165013f9d5de0
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-16 14:59:18 +0700

    add lateinit and lazy properti

---
+++ src/main/kotlin/OOP/4-lateinit-lazyProperti.kt
@@ -0,0 +1,36 @@
+package OOP
+
+// Nullable
+//var name: String? = null
+//fun main() {
+//    name = "Dicoding Miaw"
+//    print(name?.length)
+//}
+
+//Hal yang perlu diperhatikan adalah lateinit harus disandingkan dengan keyword var
+//(tidak bisa menggunakan val)
+//lateinit var name: String
+//fun main() {
+//    name = "Dicoding Miaw"
+//    print(name.length)
+//}
+
+//  Memeriksa apakah variabel tersebut sudah diinisialisasi -> isInitialized
+//lateinit var name: String
+//fun main() {
+//    name = "Dicoding Miaw"
+//    if (::name.isInitialized)
+//        print(name.length)
+//    else
+//        print("Not Initialized")
+//}
+
+//  ================
+//  Lazy Properti
+//  Note: Dalam menggunakan lazy property, Anda harus menggunakan keyword val. Hal ini karena nilainya hanya dapat diinisialisasi satu kali.
+val name: String by lazy {
+    "Dicoding Miaw"
+}
+fun main() {
+    print(name.length)
+}
