96f186db8d1c2cb32a0a66f57ec5bcb15cd17fef
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-16 14:19:15 +0700

    add class

---
+++ src/main/kotlin/OOP/2-class.kt
@@ -0,0 +1,41 @@
+package OOP
+
+// Class -> blueprint
+// Untuk membuat obejek
+
+//  Class: sebuah blueprint yang terdapat properti dan fungsi di dalamnya.
+//  Properties: karakteristik dari sebuah kelas, memiliki tipe data.
+//  Methods: kemampuan atau aksi dari sebuah kelas.
+
+// Membuat sebuah class
+class Animal() {
+    var name: String = "Kucing"
+    var weight: Double = 3.2
+    var age: Int = 2
+    var isMamal: Boolean = true
+
+    fun eat() {
+        println("$name makan!")
+    }
+
+    fun sleep() {
+        println("$name tidur!")
+    }
+}
+
+fun main() {
+//    Membuat sebuah objek
+//    val nameOfObject = NameOfClass()
+    val dicodingCat = Animal()
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, Mamalia: ${dicodingCat.isMamal}")
+    dicodingCat.eat()
+    dicodingCat.sleep()
+
+    dicodingCat.name = "Roki"
+    dicodingCat.weight = 6.0
+    dicodingCat.age = 3
+    println("Nama: ${dicodingCat.name}, Berat: ${dicodingCat.weight}, Umur: ${dicodingCat.age}, Mamalia: ${dicodingCat.isMamal}")
+    dicodingCat.eat()
+    dicodingCat.sleep()
+
+}
