16dc1cb29b9e26c7f82466c1f7a061ff82d638a0
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-16 14:47:34 +0700

    add properties

---
+++ src/main/kotlin/OOP/3-properties.kt
@@ -0,0 +1,34 @@
+package OOP
+
+// getter = pengambilan
+// setter = penyetel
+
+//class Animal2{
+//    var name: String = "Dicoding Miaw"
+//}
+//
+//fun main(){
+//    val dicodingCat = Animal2()
+//    println("Nama: ${dicodingCat.name}" )
+//    dicodingCat.name = "Goose"
+//    println("Nama: ${dicodingCat.name}")
+//}
+
+class Animal2{
+    var name: String = "Dicoding Miaw"
+        get(){
+            println("Fungsi Getter terpanggil")
+            return field
+        }
+        set(value){
+            println("Fungsi Setter terpanggil")
+            field = value
+        }
+}
+
+fun main(){
+    val dicodingCat = Animal2()
+    println("Nama: ${dicodingCat.name}" )
+    dicodingCat.name = "Goose"
+    println("Nama: ${dicodingCat.name}")
+}
