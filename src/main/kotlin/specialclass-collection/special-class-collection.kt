a30f3a22615749f05dd2bb092e6c394e8fe1dfdf
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-17 23:06:29 +0700

    add special class collection

---
+++ src/main/kotlin/specialclass-collection/special-class-collection.kt
@@ -0,0 +1,14 @@
+package `specialclass-collection`
+
+// SPECIAL CLASS
+// 1. Data Class: dirancang khusus untuk menyimpan data. Otomatis terdapat fungsi equals(), hashCode(), toString(), dan copy().
+// 2. Nested Class: membuat Class di dalam Class tanpa akses ke kelas induk.
+// 3. Inner Class: membuat Class di dalam Class dengan akses ke kelas induk.
+// 4. Enum Class: tipe data dengan sekumpulan nilai konstan yang telah ditentukan.
+// 5. Sealed Class: digunakan untuk pembatasan hierarki class.
+
+// COLLECTION
+// 1. List: untuk menyimpan nilai bebas dengan index (boleh sama).
+// 2. Set: untuk menyimpan nilai yang unik (tidak boleh sama).
+// 3. Map: untuk menyimpan dengan format key to value (key tidak boleh sama).
+
