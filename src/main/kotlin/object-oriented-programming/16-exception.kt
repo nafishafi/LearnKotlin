eb5b7bd39d0b7c324e9806c7b5b295c29003dfcc
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-17 14:03:53 +0700

    add exception

---
+++ src/main/kotlin/object-oriented-programming/16-exception.kt
@@ -0,0 +1,18 @@
+package `object-oriented-programming`
+
+// Exception adalah event (kejadian) yang dapat mengacaukan jalannya suatu program
+
+fun main() {
+//    ArithmeticException
+//    val someValue = 6
+//    println(someValue / 0)
+//
+////    NumberFormatException
+//    val someStringValue = "18.0"
+//    println(someStringValue.toInt())
+//
+////    NullPointerException
+//    val someNullValue: String? = null
+//    val someMustNotNullValue: String = someNullValue!!
+//    println(someMustNotNullValue)
+}
