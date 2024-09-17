d8e12b21a88b7a4f7dcb3e371e2e680bd802f87b
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-17 13:13:06 +0700

    add import package

---
+++ src/main/kotlin/OOP/15-package-import.kt
@@ -0,0 +1,24 @@
+package OOP
+
+import kotlin.math.PI
+import kotlin.math.cos as cosinus
+import kotlin.math.sqrt as akar
+
+// Import digunakan untuk memanggil kelas yang telah kita buat di file lain
+// seperti import kotlin.random.Random
+
+// Kita juga dapat mengganti nama sebuah kelas, fungsi atau variabel yang kita import
+// dengan menggunakan alias yang direpresentasikan dengan kata kunci as.
+
+// import kotlin.math.PI
+// import kotlin.math.cos as cosinus
+// import kotlin.math.sqrt as akar
+
+// Kita dapat mengimpor seluruh kelas, fungsi dan variabel yang berada pada suatu package dengan menggunakan tanda  * pada akhir package tersebut.
+// import kotlin.math.*
+
+fun main(){
+    println(PI)
+    println(cosinus(120.0))
+    println(akar(9.0))
+}
