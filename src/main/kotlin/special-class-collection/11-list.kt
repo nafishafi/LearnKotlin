416d1a26c31ab20ffad189ccd349e5f769ba06a9
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-18 20:12:17 +0700

    add mutableList

--- src/main/kotlin/special-class-collection/11-list.kt
+++ src/main/kotlin/special-class-collection/11-list.kt
@@ -1,15 +1,15 @@
 package `special-class-collection`
 
-// Dengan List kita dapat menyimpan banyak data menjadi satu objek dan bermacam-macam tipe
-// Int, String, Boolean, dll
-
-val numberList = listOf(1, 2, 3, 4 , 5) // tipe integer
-val charList = listOf('a', 'b', 'c') // tipe char
-val anyList = listOf('a', "Kotlin", 3, true) // tipe any
-
-//Bahkan kita pun bisa memasukkan sebuah data class ke dalam List tersebut.
-//val anyList = listOf('a', "Kotlin", 3, true, User())
 
 fun main() {
+    // Dengan List kita dapat menyimpan banyak data menjadi satu objek dan bermacam-macam tipe
+    // Int, String, Boolean, dll
+
+    val numberList = listOf(1, 2, 3, 4 , 5) // tipe integer
+    val charList = listOf('a', 'b', 'c') // tipe char
+    val anyList = listOf('a', "Kotlin", 3, true) // tipe any
+
+    //Bahkan kita pun bisa memasukkan sebuah data class ke dalam List tersebut.
+    //val anyList = listOf('a', "Kotlin", 3, true, User())
     println(anyList[2])
 }
---
+++ src/main/kotlin/special-class-collection/12-mutableList.kt
@@ -0,0 +1,16 @@
+package `special-class-collection`
+
+// List tidak bisa di manipulasi seperti add, remove, change data didalamnya
+// Jika ingin list yang bisa di manipulasi maka gunakan mutableList
+
+
+fun main() {
+    val anyList2 = mutableListOf('a', "Kotlin", 3, true)
+
+    anyList2.add('d') // menambah item di akhir list
+    anyList2.add(1, "love") // menambah item pada indeks ke-1
+    anyList2[3] = false // mengubah nilai item pada indeks ke-3
+    anyList2.removeAt(0) // menghapus item pada indeks ke-0
+
+    println(anyList2)
+}
