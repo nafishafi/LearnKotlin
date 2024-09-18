e770787cf1bfa8ead5d110757cea71c0d10662dd
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-18 23:08:00 +0700

    add collection operation

---
+++ src/main/kotlin/special-class-collection/16-collection-operation.kt
@@ -0,0 +1,39 @@
+package `special-class-collection`
+
+// Operasi untuk mengurutkan, memfilter data dll
+
+
+fun main() {
+    val numberList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+// =============
+// FILTER - FILTER NOT
+    val evenList = numberList.filter { it % 2 == 0 }
+    val notEvenList = numberList.filterNot { it % 2 == 0 }
+
+    println(evenList)
+    println(notEvenList)
+
+//  =============
+//  MAP
+    val multipliedBy5 = numberList.map { it * 5 } // setiap item di list akan dikali 5
+    println(multipliedBy5)
+
+//  =============
+//  COUNT
+    println(numberList.count()) // Menampilkan jumlah item yang ada di dalamnya
+    println(numberList.count {it % 3 == 0}) // Menampilkan jumlah item kelipatan 3
+
+//  =============
+//  find(), firstOrNull(), & lastOrNull()
+    val firstOddNumber = numberList.find { it % 2 == 1 } // Mencari angka ganjil pertama dari list
+    val firstOrNullNumber = numberList.firstOrNull { it % 2 == 3 } // Mencari angka ganjil ketiga dari list
+//  Jika di dalam collection tidak ditemukan data yang sesuai, fungsi akan mengembalikan nilai null.
+
+    println(firstOddNumber)
+    println(firstOrNullNumber)
+
+//  lastOrNull() -> Mencari item terakhir
+
+
+}
