f721178ba3ce8e96f9c37c9f18a96c6f41ccf7f6
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-19 22:33:08 +0700

    add lambda with receiver

--- src/main/kotlin/special-class-collection/27-lamba-with-receiver.kt
+++ src/main/kotlin/special-class-collection/27-lamba-with-receiver.kt
@@ -1,2 +1,38 @@
 package `special-class-collection`
 
+fun main() {
+//     SEBELUM
+//    val message = buildString()
+
+//     SESUDAH
+
+    val message = buildString {
+        append("Hello ")
+        append("from ")
+        append("lambda ")
+    }
+
+    println(message)
+}
+
+// ==============
+// SEBELUM
+//fun buildString(): String {
+//    val stringBuilder = StringBuilder()
+//    stringBuilder.append("Hello ")
+//    stringBuilder.append("from ")
+//    stringBuilder.append("lambda")
+//    return stringBuilder.toString()
+//}
+
+// Kode di atas merupakan contoh kode StringBuilder yang digunakan untuk menambahkan kata satu per satu.
+// Jika dilihat kode ini cukup panjang tidak fleksibel.
+// Jika Anda ingin membuat menambahkan kata baru harus di dalam kode tersebut dan mengulang-ulang kata stringBuilder.
+
+// ===============
+// SESUDAH
+fun buildString(action: StringBuilder.() -> Unit): String {
+    val stringBuilder = StringBuilder()
+    stringBuilder.action()
+    return stringBuilder.toString()
+}
