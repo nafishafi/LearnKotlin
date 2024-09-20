aaf8dc9b33755734189d36957e9e1663d4f88bd7
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-20 10:39:16 +0700

    add member references

--- src/main/kotlin/special-class-collection/31-member-references.kt
+++ src/main/kotlin/special-class-collection/31-member-references.kt
@@ -1,5 +1,8 @@
 package `special-class-collection`
 
+// Property References
+var messageProperty = "Kotlin"
+
 fun main() {
 //  Function References
 //  Dengan menggunakan operator :: kita bisa menggunakannya sebagai instances dari function type
@@ -18,7 +21,18 @@ fun main() {
 //    fun Int.isEvenNumber() = this % 2 == 0
 
 //   =============
+//    Property References
+//
+//    Dengan Operator (::), kita bisa mengakses apa yang menjadi bagian dari
+//    properti tersebut seperti nama, fungsi setter getter, dll.
+    println(::messageProperty.name)
+    println(::messageProperty.get())
+
+    ::messageProperty.set("Kotlin Academy")
+
+    println(::messageProperty.get())
 }
+
 // Reflection mekanism
     val sum: (Int, Int) -> Int = ::count
     fun count(valueA: Int, valueB
