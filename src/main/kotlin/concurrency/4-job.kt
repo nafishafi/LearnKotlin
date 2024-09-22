04aad54613c001af6f87e5e970058e9de1d6b027
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-22 22:26:04 +0700

    add start job

--- src/main/kotlin/concurrency/4-job.kt
+++ src/main/kotlin/concurrency/4-job.kt
@@ -1,5 +1,7 @@
 package concurrency
 
+import kotlinx.coroutines.*
+
 // Sebagai developer, tentunya kita menginginkan tetap bisa mengakses fungsi yang sudah dijalankan.
 // Misalnya, ketika kita ingin membatalkan tugasnya atau memberikan instruksi tambahan ketika fungsi tersebut telah mencapai kondisi tertentu.
 // Untuk bisa melakukannya, Anda perlu memahami tentang Job dan Deferred pada coroutines.
@@ -15,3 +17,56 @@ package concurrency
 // Completed = Ketika job sudah tidak berjalan lagi. Ini berlaku untuk job yang berakhir secara normal, dibatalkan, ataupun karena suatu pengecualian.
 // Cancelling = Suatu kondisi ketika fungsi cancel() dipanggil pada job yang sedang aktif dan memerlukan waktu untuk pembatalan tersebut selesai.
 // Cancelled = Keadaan yang dimiliki oleh sebuah job yang sudah berhasil dibatalkan. Perlu diketahui bahwa job yang dibatalkan juga dapat dianggap sebagai Completed job.
+
+
+////menggunakan launch():
+//fun main() = runBlocking {
+//    val job = launch {
+//        // Do background task here
+//    }
+//}
+//
+////menggunakan Job():
+//fun main() = runBlocking {
+//    val job = Job()
+//}
+
+// Setelah diinisialisasikan, job akan memiliki state New dan akan langsung dijalankan.
+// Jika Anda ingin membuat sebuah job tanpa langsung menjalankannya, Anda bisa memanfaatkan CoroutineStart.LAZY
+
+//fun main() = runBlocking {
+//    val job = launch(start = CoroutineStart.LAZY) {
+//        TODO("Not implemented yet!")
+//    }
+//}
+
+// ===========
+// Menjalankan Job
+
+// fungsi start()
+fun main() = runBlocking {
+    val job = launch(start = CoroutineStart.LAZY) {
+        delay(1000L)
+        println("Start new job!")
+    }
+
+    job.start()
+    println("Other task")
+}
+
+// Atau bisa juga dengan menggunakan fungsi join()
+//fun main() = runBlocking {
+//    val job = launch(start = CoroutineStart.LAZY) {
+//        delay(1000L)
+//        println("Start new job!")
+//    }
+//
+//    job.join()
+//    println("Other task")
+//}
+
+// Perbedaan dari keduanya adalah bahwa yang start() akan memulai job tanpa harus menunggu job tersebut selesai,
+// sedangkan join() akan menunda eksekusi sampai job selesai.
+
+
+// Setelah dijalankan,  job akan memiliki state Active.
