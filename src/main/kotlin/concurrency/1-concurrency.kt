4aaf5e782227da12bbc02d5eaa2baf422e430898
Muhammad Nafis Hafi <nafishafi17@students.unnes.ac.id>
2024-09-22 17:18:48 +0700

    add perbedaan concurrency dan parallelism

--- src/main/kotlin/concurrency/1-concurrency.kt
+++ src/main/kotlin/concurrency/1-concurrency.kt
@@ -1,3 +1,12 @@
 package concurrency
 
 // Concurrency adalah beberapa proses yang terjadi secara bersamaan dalam suatu sistem.
+
+// Perbedaan
+// Concurrency terjadi apabila terdapat 2 (dua) atau lebih proses yang tumpang tindih dalam satu waktu.
+// Parallelism terjadi ketika 2 (dua) proses dijalankan pada titik waktu yang sama persis.
+
+// Contoh:
+// 1 pelayan melayani 5 pelangggan scr bersamaan untuk membuat kopi dengan langkah yang bbrp sama
+// 2 pelayan membagi tugas
+
